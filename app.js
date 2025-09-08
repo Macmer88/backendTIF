30600e976f789a48ef57b91ab6ec7e220c25a187
Maximiliano Merele <el.mazi.tgo@gmail.com>
2025-09-08 19:26:52 -0300

    Creacion del primer servidor con express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.type('text/plain');
+    res.status(200);
+    res.send('HOla manga de gorreados!!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
