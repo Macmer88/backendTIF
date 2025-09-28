978bbb4accec4f9b2d6ada592a1a2a0b647bec14
Maximiliano Merele <el.mazi.tgo@gmail.com>
2025-09-28 18:19:55 -0300

    creacion de funcion d ebusqueda por id, y busqueda general con filtros

--- src/databases/modelo_salones.js
+++ src/databases/modelo_salones.js
@@ -1,6 +1,23 @@
 import { pool } from '../config/db.js';
 
-export async function getAllSalones() {
-    const [rows] = await pool.query('SELECT * FROM salones');
+export async function salonesConFiltro(activo, ordenar, desc, limit, offset) {
+    let query = 'SELECT * FROM salones WHERE activo = ?';
+    const params = [activo];
+
+    const columnasValidas = ['titulo', 'importe', 'capacidad'];
+    if (ordenar && columnasValidas.includes(ordenar)) {
+        query += ` ORDER BY ${ordenar}`;
+        if (desc) query += ' DESC';
+    }
+
+    query += ' LIMIT ? OFFSET ?';
+    params.push(limit, offset);
+
+    const [rows] = await pool.query(query, params);
     return rows;
+}
+
+export async function salonesPorId(id){
+    const [rows] = await pool.query('SELECT * FROM salones WHERE salon_id = ?', [id]);
+    return rows[0];
 }
