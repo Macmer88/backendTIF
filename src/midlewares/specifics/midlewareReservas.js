a3615fe4d7505a78e8ea72427ad7ee2d9ead235e
Maximiliano Merele <el.mazi.tgo@gmail.com>
2025-10-12 23:25:14 -0300

    correccion en el midleware de fechas validas

--- src/midlewares/specifics/midlewareReservas.js
+++ src/midlewares/specifics/midlewareReservas.js
@@ -15,11 +15,27 @@ export async function validarFecha(req, res, next) {
 export async function validarExtension(req, res, next) {
     const { fecha_reserva } = req.body;
     const fecha_maxima = new Date();
-    fecha_maxima.setMonth() + 6;
+    fecha_maxima.setMonth(fecha_maxima.getMonth() + 6);
     try {
         if (new Date(fecha_reserva) > fecha_maxima) {
             const error = new Error('No se puede reservar con más de 6 meses de anticipación');
             error.statusCode = 400;
+            return next(error);
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
+
+export async function validarAnticipacion(req, res, next) {
+    const { fecha_reserva } = req.body;
+    const antelacion = new Date();
+    antelacion.setHours(antelacion.getHours() + 48);
+    try {
+        if (new Date(fecha_reserva) < antelacion) {
+            const error = new Error('Debe reservar con al menos 48 horas de anticipación');
+            error.statusCode = 409;
             return next(error);
         }
         next();
